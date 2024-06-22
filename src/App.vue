ea3f0377eac3feb5ea19e7b2cbd4bbab6bf11686
steamdroid <steamdroid77@gmail.com>
2024-06-22 18:57:19 +0300

    add floating up button

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <AppHeader />
-  <main x-data>
+  <main>
     <div class="container">
       <BooksTable />
     </div>
   </main>
+  <UpButton />
 </template>
 <script setup>
 import AppHeader from './components/AppHeader.vue';
 import BooksTable from './components/BooksTable.vue';
+import UpButton from './components/UpButton.vue';
 </script>
 <style lang="scss"></style>
---
+++ src/components/UpButton.vue
@@ -0,0 +1,52 @@
+<template>
+  <button
+    type="button"
+    class="up"
+    :class="{
+      'up--visible': showButton
+    }"
+    @click="backToTop"
+    title="Наверх"
+  ></button>
+</template>
+<script setup>
+import { computed } from 'vue';
+import { useWindowScroll } from '@vueuse/core';
+
+const { y } = useWindowScroll();
+
+const showButton = computed(() => y.value > 100);
+
+const backToTop = () => (y.value = 0);
+</script>
+<style scoped>
+.up {
+  padding: 0;
+  margin: 0;
+  display: none;
+  position: fixed;
+  bottom: 15px;
+  right: 15px;
+  width: 30px;
+  height: 30px;
+  background-color: #0172ad;
+  border-radius: 3px;
+  cursor: pointer;
+}
+.up::after {
+  content: '';
+  width: 15px;
+  height: 15px;
+  border: 2px solid #ffffff;
+  border-bottom-color: transparent;
+  border-right-color: transparent;
+  transform: rotate(45deg);
+  top: 12px;
+  right: 6px;
+  display: block;
+  position: absolute;
+}
+.up--visible {
+  display: block !important;
+}
+</style>
